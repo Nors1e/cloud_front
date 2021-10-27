dd2ed2b9a0ce046e144782c03d6a95b5f1294162
Norsie <hisurfingc@yahoo.com>
2021-10-26 23:47:27 -0700

    added bootstrap templating, fixed template inheritance

--- backend.py
+++ backend.py
@@ -8,20 +8,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html", content="Testing")
+    return render_template("index.html")
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
-    else:
-        return render_template("base.html")
+# @app.route("/login", methods=["POST", "GET"])
+# def login():
+#     if request.method == "POST":
+#         user = request.form["nm"]
+#         return redirect(url_for("user", usr=user))
+#     else:
+#         return render_template("base.html")
 
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+# @app.route("/<usr>")
+# def user(usr):
+#     return f"<h1>{usr}</h1>"
 
 
 if __name__ == "__main__":
