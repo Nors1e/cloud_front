8f9b4b52e9e2c7ce0198a3bec7137eedcaed67a9
Caleb Stout <hisurfingc@yahoo.com>
2022-12-06 22:59:44 -0700

    Updating Roadmap
    Respecing the Flask application

--- backend.py
+++ backend.py
@@ -5,53 +5,7 @@ from markupsafe import escape
 # Instance of a flask web application
 app = Flask(__name__)
 
-
+# Add in the 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-#fin
-#fixed
-# @app.route("/login", methods=["POST", "GET"])
-# def login():
-#     if request.method == "POST":
-#         user = request.form["nm"]
-#         return redirect(url_for("user", usr=user))
-#     else:
-#         return render_template("base.html")
-
-
-# @app.route("/<usr>")
-# def user(usr):
-#     return f"<h1>{usr}</h1>"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-# Adding new line
-
-
-# # If user tries to access this page they get redirected to another page if they do not have privs
-# @app.route("/admin")
-# def admin():
-#     return redirect(url_for("home"))
-
-# # Registers unique url and feeds it into page
-# @app.route("/<name>")
-# def user(name):
-#     return f"Hello <h1>{name}</h1>!"
-
-# # Routing
-# @app.route("/")
-# def homepage(name=None):
-#     return render_template('index.html', name=name)
-
-# # Route to the home page
-# @app.route("/home")
-# def spice():
-#     return "I'm back"
-
-# @app.route('/home/user/<username>')
-#     return f'User {escape(username)}'
